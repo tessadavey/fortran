0a1b910f507d47224108419eb99f93f00127244a
Tessa Davey <tessadavey@outlook.com>
2018-04-06 16:54:59 +0900

    edits

--- Lecture2/enshu_1.f90
+++ Lecture2/enshu_1.f90
@@ -1,14 +1,13 @@
 program enshu_1
   implicit none
-
   integer i
   real ca_result = 0.0
-  real n = 100
-  real area(n), f(n)
+  integer N = 100
+  real area(N), f(N)
   real a = 100.0
-  real dx = a/n
-  do i = 1,100
-    f(i) = a**2-(dx*i)**2
+  real dx = a/N
+  do i = 1, 100
+    f(i) = a**2 - (dx*i)**2
     area(i) = f(i)*dx
     ca_result = ca_result+area(i)
   enddo
