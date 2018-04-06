c7717337486b82f21525fb966055755a6efbf6c7
Tessa Davey <tessadavey@outlook.com>
2018-04-06 16:30:15 +0900

    added spaces

--- Lecture2/enshu_1.f90
+++ Lecture2/enshu_1.f90
@@ -1,15 +1,15 @@
 program enshu_1
   implicit none
   integer i
-  real ca_result=0.0
+  real ca_result = 0.0
   integer n = 100
   real area(n), f(n)
-  real a=100.0
-  real dx=a/n
-  do i=1,100
-    f(i)=a**2-(dx*i)**2
-    area(i)=f(i)*dx
-    ca_result=ca_result+area(i)
+  real a = 100.0
+  real dx = a/n
+  do i = 1,100
+    f(i) = a**2-(dx*i)**2
+    area(i) = f(i)*dx
+    ca_result = ca_result+area(i)
   enddo
   stop
 end program enshu_1
