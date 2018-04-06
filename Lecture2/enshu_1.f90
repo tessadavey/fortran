ab628aa2cca9742cf362484b9e89b705d3ee4f60
Tessa Davey <tessadavey@outlook.com>
2018-04-06 17:05:50 +0900

    Added :: in declarations

--- Lecture2/enshu_1.f90
+++ Lecture2/enshu_1.f90
@@ -1,11 +1,11 @@
 program enshu_1
   implicit none
-  integer i
-  real result = 0.0
-  integer N = 100
-  real area(N), f(N)
-  real a = 100.0
-  real dx = a/N
+  integer :: i
+  real :: result = 0.0
+  integer :: N = 100
+  real :: area(N), f(N)
+  real :: a = 100.0
+  real :: dx = a/N
   do i = 1, 100
     f(i) = a**2 - (dx*i)**2
     area(i) = f(i)*dx
